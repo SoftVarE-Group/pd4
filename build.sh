fa22c3f37c681d4a3f68260d58b8673e1fc60839
Max <caradhras11@gmail.com>
2023-09-26 14:54:58 +0200

    next

--- build.sh
+++ build.sh
@@ -23,6 +23,7 @@ do
 done
 
 curRep=$PWD
+echo building glucose
 if [ $opt -eq 1 ]
 then
    if ! [ -f 3rdParty/glucose-3.0/core/lib_debug.a ]
@@ -46,6 +47,8 @@ else
 fi
 
 
+
+echo building kahypar
 if ! [ -f 3rdParty/kahypar/build/lib/libkahypar.a ]
 then
     cd $curRep
@@ -57,6 +60,8 @@ then
 fi
 
 
+
+echo building cryptominisat
 if ! [ -f ./3rdParty/cryptominisat/build/lib/libcrytominisat5.so]
 then
     cd $curRep
@@ -66,6 +71,8 @@ then
     cmake -DCMAKE_CXX_FLAGS="-include cstdint"  .. 
     make -j12
 fi
+
+echo building louvain-community
 if ! [ -f ./3rdParty/louvain-community/build/lib/liblouvain_communities.so  ]
 then
     cd $curRep
@@ -75,6 +82,8 @@ then
     cmake -DCMAKE_CXX_FLAGS="-include cstdint"  .. 
     make -j12
 fi
+
+echo building arjun
 if ! [ -f ./3rdParty/arjun/build/lib/libarjun.so  ]
 then
     cd $curRep
@@ -85,13 +94,15 @@ then
     make -j12
 fi
 
+
+echo building mt-kahypar
 if ! [ -f 3rdParty/mt-kahypar/build/lib/libmtkahypar.so ]
 then
     cd $curRep
     cd 3rdParty/mt-kahypar
     mkdir -p build
     cd build
-    cmake .. -DCMAKE_BUILD_TYPE=RELEASE  -DKAHYPAR_DOWNLOAD_TBB=On
+    cmake .. -DCMAKE_BUILD_TYPE=RELEASE  -DKAHYPAR_DOWNLOAD_TBB=On -DKAHYPAR_DOWNLOAD_BOOST=On
     cmake --build . --target mtkahypar
 fi
 
